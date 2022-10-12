03a4aceb0214dda82c41587a17b465ab0526fb19
xhorna16 <xhorna16@stud.feec.vutbr.cz>
2022-10-12 16:44:03 +0200

    cv03

---
+++ cv031/Core/Inc/sct.h
@@ -0,0 +1,17 @@
+/*
+ * sct.h
+ *
+ *  Created on: 12. 10. 2022
+ *      Author: xhorna16
+ */
+
+#ifndef SCT_H_
+#define SCT_H_
+
+
+void sct_init(void);
+void sct_led(uint32_t value);
+void sct_value(uint16_t value);
+
+
+#endif /* SCT_H_ */
